8bc6df819aa579d7505110bd873f40470f0ec1f6
uncasa <uncasaa@gmail.com>
2021-11-17 15:58:34 +0100

    array function practise

---
+++ script.js
@@ -0,0 +1,60 @@
+const generateArray = function (amount) {
+    let returnArray = [];
+/*
+    let amountConverted = parseInt(amount);
+    amount = amountConverted;
+*/
+    amount = parseInt(amount);
+    //console.log(amount);
+    //console.log(typeof amount);
+
+    //Number.isNaN(amount) !== true
+    if (!Number.isNaN(amount)){
+        for (let i = 0; i < amount; i++) {
+            returnArray.push(`${i}`);
+            //onsole.log(returnArray);
+        } 
+    }   else {
+            returnArray.push("error");        
+    }
+
+    
+    //console.log(returnArray);
+    return returnArray;
+}
+
+function loadEvent() {
+    console.log("the page has loaded");
+
+    //console.log(generateArray("kiskutya"));
+    //console.log(generateArray("1 kiskutya"));
+    //console.log(generateArray(100));
+    //console.log(generateArray("100"));
+    //console.log(generateArray([100]));
+// string nem mukodik, ures tombot ad vissza. 
+
+    const root = document.getElementById("root");
+    const list = generateArray(6);
+    console.log(list);
+
+    if (list[0] !== "error") {
+        for (const item of list) {
+            root.insertAdjacentHTML("beforeend", `<div>${item}</div>`)
+        }
+    }
+
+};
+
+window.addEventListener("load", loadEvent);
+
+/*
+window.addEventListener("load", function(){
+    console.log("the page loaded")
+    Ez is egy valid megoldás. 
+    
+window.addEventListener("load", () => {
+    Arrow function, ez is valid.
+    _  Ezt használva nem akarunk a zarojelbe parametert bevinni. 
+
+    });
+*/
